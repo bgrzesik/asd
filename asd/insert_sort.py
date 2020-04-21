5bf28991901565be74754c4ceef271ead832494d
Bartłomiej Grzesik <grzechovsky@gmail.com>
2020-04-21 19:01:44 +0200

    Implement insert sort on linked lists Add assert to insert sort

--- asd/insert_sort.py
+++ asd/insert_sort.py
@@ -11,7 +11,49 @@ def insert_sort(arr):
         arr[j + 1] = val
 
 
+class Node:
+    def __init__(self, val=None):
+        self.next = None
+        self.val = None
+
+
+def insert_sort_linked_list(head):
+    node = head
+    res = Node()
+
+    while node.next is not None:
+        i = node.next
+        node.next = node.next.next
+
+        j = res
+        while j.next is not None and j.next.val < i.val:
+            j = j.next
+
+        i.next = j.next
+        j.next = i
+
+    head.next = res.next
+    return head
+
+
 if __name__ == "__main__":
     tab = [0, 0, 2, 3, 4, 1, 3, 3, 3, 4, 5]
     insert_sort(tab)
-    print(tab, list(sorted([0, 0, 2, 3, 4, 1, 3, 3, 3, 4, 5])))
+    assert tab == list(sorted([0, 0, 2, 3, 4, 1, 3, 3, 3, 4, 5]))
+
+    head = Node()
+    for i in [0, 2, 4, 6, 7, 8, 3, 4, 5, 3, 1, 12, 24, 56]:
+        node = Node()
+        node.val = i
+        node.next = head.next
+        head.next = node
+
+    insert_sort_linked_list(head)
+
+    arr = []
+    while head.next is not None:
+        node = head.next
+        head.next = head.next.next
+        arr.append(node.val)
+
+    assert arr == [0, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 12, 24, 56]
